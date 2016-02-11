7065e52defa56bdea45f2e4f66259802432b6e69
Mohit Gupta <mo@mohitgupta.ca>
2016-02-11 13:01:48 -0500

    Moved secrets out of the settings file. Secrets is not tracked by git.

--- shrutiwedsmohit/settings.py
+++ shrutiwedsmohit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from . import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '07cy*$jtski)y@)71&qcmy!-kd1wr$a4=hox4m5&gl3nd9a@&d'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
